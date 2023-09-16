a36d32a6329af1e7519e0497dd5cb89ec9010df3
Oleksii Ishchenko <optionalzirelleariz>
2023-09-16 01:34:02 -0600

    second update

--- ex47/Program.cs
+++ ex47/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5  7   -2 -0,2
+//  1 -3,3   8 -9,9
+//  8  7,8 -7,1  9
+
+
+int SetNumber(string input = " ")
+{
+    Console.WriteLine($"Choose the {input}");
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+
+int[,] FillMatrix(int row, int columns, int min, int max)
+{
+    Random rand = new Random();
+    int[,] dbArray = new int[row,columns];
+    for(int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            dbArray[i , j] = rand.Next(min , max + 1);
+        }
+    }
+    return dbArray;
+}
+
+static void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 4}");
+        }
+        Console.WriteLine();
+    } 
+}
+
+int row = SetNumber("row");
+int columns = SetNumber("columns");
+int minValue = SetNumber("minValue");
+int maxValue = SetNumber("maxValue");
+
+int[,] newMatrix = FillMatrix(row, columns, minValue, maxValue);
+
+PrintMatrix(newMatrix);
--- ex50IsThereElement/Program.cs
+++ ex50IsThereElement/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int SetNumber(string input = " ")
+{
+    Console.WriteLine($"Choose the {input}");
+    int answer = Convert.ToInt32(Console.ReadLine());
+    return answer;
+}
+
+bool WhichPosition(int[,] sequence, int findRow, int findColumn)
+{
+    int numRows = sequence.GetLength(0);
+    int numColumns = sequence.GetLength(1);
+    if (findRow >= 0 && numRows > findRow && findColumn >= 0 && numColumns > findColumn)
+    {
+        return true;
+    }
+    return false;
+}
+
+int[,] FillMatrix(int row, int columns, int min, int max)
+{
+    Random rand = new Random();
+    int[,] dbArray = new int[row,columns];
+    for(int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            dbArray[i , j] = rand.Next(min , max + 1);
+        }
+    }
+    return dbArray;
+}
+
+static void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j], 4}");
+        }
+        Console.WriteLine();
+    } 
+}
+
+
+int[,] newMatrix = FillMatrix(4, 4, 1, 10);
+
+PrintMatrix(newMatrix);
+
+int findRow = SetNumber("row position, you want to find: ") - 1;
+int findColumn = SetNumber("column position, you want to find: ") - 1;
+
+bool result = WhichPosition(newMatrix, findRow, findColumn);
+Console.WriteLine(result ? (object)newMatrix[findRow, findColumn] : "There is no such position");
+
